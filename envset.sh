d51c077411092491d871180d7c6c60c25e42f444
kyHwangs <kyuyeong.hwang@gmail.com>
2024-04-26 17:39:21 +0200

    Clean script

--- envset.sh
+++ envset.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # When using CVMFS (no X11)
-#source /cvmfs/sft.cern.ch/lcg/views/LCG_102/arm64-mac12-clang131-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102/arm64-mac12-clang131-opt/setup.sh
 
 # When using local ROOT (X11 activated)
-source /Users/yhep/scratch/DQM/root-6.26.04/install/bin/thisroot.sh
+# source /Users/yhep/scratch/DQM/root-6.26.04/install/bin/thisroot.sh
 
 export INSTALL_DIR_PATH=$PWD/install
 
