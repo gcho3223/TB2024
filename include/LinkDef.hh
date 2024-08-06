7f1830b2c7862dd13739d9a7cff853cd1639d648
kyHwangs <kyuyeong.hwang@gmail.com>
2024-08-06 08:59:05 +0200

    skimming useless scripts

--- include/LinkDef.hh
+++ include/LinkDef.hh
@@ -21,9 +21,4 @@
 #pragma link C++ class TBmonit < TBwaveform> + ;
 #pragma link C++ class TBmonit < TBfastmode> + ;
 
-#pragma link C++ class TBmcppmt < TBwaveform> + ;
-#pragma link C++ class TBmcppmt < TBfastmode> + ;
-
-#pragma link C++ class TBntupler;
-
 #endif
